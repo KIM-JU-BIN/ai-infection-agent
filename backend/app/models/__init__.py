e8612f3d7ed9cf4bcbd33ba17fe47a54c8df0052
b22615014 <b22615014@gmail.com>
2026-08-12 17:55:28 +0900

    refactor: 백엔드 및 RAG 모듈 전체 삭제 및 구조 재설계 준비

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .patient import Patient
-from .location import Location
-from .access_log import AccessLog
-from .emr_record import EMRRecord
-from .investigation_result import InvestigationResult
